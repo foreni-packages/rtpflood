cd4ad8ac1b3644bdad64ec0a8878898e3bdeca65
Devon Kearns <dookie@kali.org>
2012-12-30 17:52:35 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(rtpflood):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="rtpflood" command="/usr/bin/rtpflood"
--- debian/rtpflood.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the rtpflood package
-#
-0 4	* * *	root	[ -x /usr/bin/rtpflood_maintenance ] && /usr/bin/rtpflood_maintenance
